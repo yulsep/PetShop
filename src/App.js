121e46305767d3397855632daf0d267715258a2d
Your Name <yulimarcela99@gmail.com>
2023-05-09 21:12:47 -0500

    nav bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import './assets/css/base/base.css';
-import './assets/css/componentes/card.css'
+import "./assets/css/base/base.css";
+import "./assets/css/componentes/card.css";
+import Home from "./pages/Home.jsx";
+import Sobre from "./pages/Sobre.jsx";
+import Page404 from "./pages/Page404.jsx";
+import Header from "./components/Header";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sobre" element={<Sobre />} />
+          <Route path="*" element={<Page404 />} />
+        </Routes>
+      </Router>
     </>
   );
 }
