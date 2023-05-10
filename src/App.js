581f3ef709a6f00bfa04df445bcc5df2f49dc850
Your Name <yulimarcela99@gmail.com>
2023-05-09 22:21:01 -0500

    Categorias de noticias

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Home from "./pages/Home.jsx";
 import Sobre from "./pages/Sobre.jsx";
 import Page404 from "./pages/Page404.jsx";
 import Header from "./components/Header";
+import Post from "./pages/Post";
+import Categoria from "./pages/Categoria";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -15,6 +17,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/sobre" element={<Sobre />} />
+          <Route path="/posts/:id" element={<Post />} />
+          <Route path="/categoria/:id" element={<Categoria />} />
           <Route path="*" element={<Page404 />} />
         </Routes>
       </Router>
